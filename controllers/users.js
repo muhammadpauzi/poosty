36391a22dff8d7b4242be780b043b42a6d95e172
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-11-21 17:40:59 +0700

    feat: implement detail page of user

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,30 @@
-const { USERS_TITLE } = require('../constants');
+const { USERS_TITLE, USER_DETAIL_TITLE } = require('../constants');
 const { renderWithUserDataAndFlash } = require('../utils');
 const { User, Like, Post } = require('../models');
 
 const users = async (req, res) => {
     try {
-        const users = await User.findAll({
-            include: [Like, Post],
-            order: [
-                ['username', 'ASC']
-            ]
-        });
+        const users = await User.findAll({ order: [['username', 'ASC']] });
         return renderWithUserDataAndFlash(req, res, 'users/users', { title: USERS_TITLE, users });
     } catch (error) {
         console.log(error);
     }
 }
 
-module.exports = { users };
+const detail = async (req, res) => {
+    const { username } = req.params;
+    try {
+        const user = await User.findOne({
+            where: { username }, include: { model: Post, include: Like }
+        });
+        if (!user) {
+            req.flash('error', 'User does not exist,');
+            return res.redirect('/posts');
+        }
+        return renderWithUserDataAndFlash(req, res, 'users/detail', { title: username + USER_DETAIL_TITLE, user, likes: user.Likes, posts: user.Posts });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { users, detail };
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
-const { users } = require('../controllers/users');
+const { users, detail } = require('../controllers/users');
 const router = require('express').Router();
 const { ensureAuth } = require('../middlewares/auth');
 
 router.get('/', ensureAuth, users);
+router.get('/:username', ensureAuth, detail);
 
 module.exports = router;
